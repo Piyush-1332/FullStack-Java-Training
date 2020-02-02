21d578c54b2c4b0b77f75708527fa3db327ed35e
Piyush-1332 <piyush1332@gmail.com>
2020-02-02 16:52:33 +0530

    Lab6Ex2 created

---
+++ capgemini/firstproject/welcome/Lab3Ex4.java
@@ -0,0 +1,9 @@
+package capgemini.firstproject.welcome;
+
+public class Lab3Ex4 {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ capgemini/firstproject/welcome/Lab6Ex2.java
@@ -0,0 +1,41 @@
+package capgemini.firstproject.welcome;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Lab6Ex2 {
+	Scanner mScan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Lab6Ex2 lab = new Lab6Ex2();
+		lab.readFile();
+	}
+	public void readFile() {
+		String line=null;
+		String fname;
+		System.out.println("Enter the name of the File with Extension : ");
+		fname = mScan.nextLine();
+		try {
+			FileReader mfile = new FileReader("C:\\Users\\SAI\\Documents\\workspace-sts-3.9.11.RELEASE\\NietApplication\\src\\capgemini\\firstproject\\demo.txt");
+			BufferedReader mb = new BufferedReader(mfile);
+			try {
+				while((line=mb.readLine())!=null) {
+					System.out.println(line);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Error reading File : "+fname);
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
